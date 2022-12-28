1be7b9b4936d479d377e7c00b2ff88558713273e
PolemarchRU <118505658+PolemarchRU@users.noreply.github.com>
2022-12-28 10:37:34 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// Copy text
+copying1.onclick = () => {
+    navigator.clipboard.writeText('Полемарх#6372')
+}
+
+copying2.onclick = () => {
+    navigator.clipboard.writeText('iusmanovvv@yandex.ru')
+}
+
+// AOS
+AOS.init({
+    once: true,
+});
